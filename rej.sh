022bc19710020238f5f580b986daa8b6f60aa7f2
Magdalena <machmara@interia.pl>
2011-12-13 19:44:15 +0100

    poprawka ifow

--- rej.sh
+++ rej.sh
@@ -1,9 +1,8 @@
-cho 'Stan ogolny 1-10'
 #!/bin/bash
 diagnoza=0;
-echo 'KLASYFIKACJA PACJENTÃW'
-echo 'Po wpisaniu danych wciÅij Enter by przejÅ dalej.'
-echo 'ImiE:'
+echo 'KLASYFIKACJA PACJENTÓW'
+echo 'Po wpisaniu danych wciœnij Enter by przejœæ dalej.'
+echo 'Imiê:'
 read imie
 echo 'Nazwisko:'
 read nazwisko
@@ -18,17 +17,17 @@ echo "STAN:$stan" >> "$nazwisko$imie"
 echo 'Przytomny? (1-tak/0-nie)'
 read przytomnosc
 diagnoza=$[$diagnoza+20*$przytomnosc];
-echo "PRZYTOMNOSC:$przytomnosc" >> $nazwisko$imie
+echo "PRZYTOMNOŒÆ:$przytomnosc" >> $nazwisko$imie
 echo 'Oddycha? (1-tak/0-nie)'
 read oddech
 diagnoza=$[$diagnoza+50*$oddech];
 echo "ODDECH:$oddech" >> $nazwisko$imie
 echo "DIAGNOZA:$diagnoza" >> "$nazwisko$imie"
 
-if [$[$diagnoza < 50]];
+if (($diagnoza<50));
 then
 mv $nazwisko$imie natychmiastowa_pomoc
-elif [$diagnoza -le 75];
+elif (($diagnoza<75));
 then
 mv $nazwisko$imie stan_posredni
 else
