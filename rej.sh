83bec183241fcb953fa5c1b814741a20520b556b
Magdalena <machmara@interia.pl>
2011-12-14 08:04:47 +0100

    zlikwidowanie podwójnego kodu

--- rej.sh
+++ rej.sh
@@ -33,38 +33,3 @@ mv $nazwisko$imie stan_posredni
 else
 mv $nazwisko$imie lagodne_schorzenie
 fi
-#!/bin/bash
-diagnoza=0;
-echo 'KLASYFIKACJA PACJENTOW'
-echo 'Po wpisaniu danych wcisnij Enter by przejsc dalej.'
-echo 'ImiE:'
-read imie
-echo 'Nazwisko:'
-read nazwisko
-echo 'Pesel:'
-read pesel
-touch "$nazwisko$imie"
-echo "PESEL:$pesel" >> "$nazwisko$imie"
-echo 'Stan ogolny 1-10'
-read stan
-diagnoza=$[$diagnoza+10*$stan];
-echo "STAN:$stan" >> "$nazwisko$imie"
-echo 'Przytomny? (1-tak/0-nie)'
-read przytomnosc
-diagnoza=$[$diagnoza+20*$przytomnosc];
-echo "PRZYTOMNOSC:$przytomnosc" >> $nazwisko$imie
-echo 'Oddycha? (1-tak/0-nie)'
-read oddech
-diagnoza=$[$diagnoza+50*$oddech];
-echo "ODDECH:$oddech" >> $nazwisko$imie
-echo "DIAGNOZA:$diagnoza" >> "$nazwisko$imie"
-
-if [$[$diagnoza < 50]];
-then
-mv $nazwisko$imie natychmiastowa_pomoc
-elif [$diagnoza -le 75];
-then
-mv $nazwisko$imie stan_posredni
-else
-mv $nazwisko$imie lagodne_schorzenie
-fi
